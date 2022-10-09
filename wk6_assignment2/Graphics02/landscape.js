d42318d252d2309eb5944dd3b5c10899a6887e68
youkeongjun <youkeongjun@naver.com>
2022-10-10 00:18:11 +0900

    dots

--- wk6_assignment2/Graphics02/landscape.js
+++ wk6_assignment2/Graphics02/landscape.js
@@ -6,7 +6,13 @@ var maxNumVertices = 3 * MaxNumTriangles;
 var index = 0;
 
 var colors = [
-    vec4(1.0,1.0,0.0,1.0) //yellow
+    vec4(1.0,1.0,0.0,1.0), //yellow
+    vec4(1.0,1.0,0.0,1.0), //yellow
+    vec4(1.0,1.0,0.0,1.0), //yellow
+    vec4(1.0,1.0,0.0,1.0), //yellow
+    vec4(1.0,1.0,0.0,1.0), //yellow
+    vec4(1.0,1.0,0.0,1.0), //yellow
+    vec4(1.0,1.0,0.0,1.0), //yellow
 ];
 
 window.onload = function init()
@@ -30,15 +36,35 @@ window.onload = function init()
     });
 
     gl.viewport(0,0,canvas.width,canvas.height);
-    gl.clearColor(0.5,0.5,0.5,1.0);
+    gl.clearColor(0.1,0.1,0.3,1.0);
 
     // Load shaders an initialize attribute buffers
-    var program = initShaders(gl,"vertext-shader","fragment-shader");
+    var program = initShaders(gl,"vertex-shader","fragment-shader");
     gl.useProgram(program);
 
     var vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER,vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, 8*maxNumVertices, gl.STATIC_DRAW);
 
-    var vPosition
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, 16*maxNumVertices, gl.STATIC_DRAW );
+
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
+
+    render();
+
+}
+
+function render(){
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.POINTS,0,index);
+
+    window.requestAnimationFrame(render);
 }
