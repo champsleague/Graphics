92152b5597a3b1487c03de96ce4304744872e6c6
youkeongjun <youkeongjun@naver.com>
2022-10-13 13:22:41 +0900

    update

--- wk7_3/Graphics02/perspective.js
+++ wk7_3/Graphics02/perspective.js
@@ -25,77 +25,6 @@ const at = vec3(0.0,0.0,0.0); //at point
 const up = vec3(0.0,1.0,0.0); //up direction
 
 
-
-
-// quad uses first index to set color for face
-function quad(a,b,c,d){
-    var vertices = [
-        vec4( -0.5, -0.5,  0.5, 1.0 ), // 0
-        vec4( -0.5,  0.5,  0.5, 1.0 ), // 1
-        vec4(  0.5,  0.5,  0.5, 1.0 ), // 2
-        vec4(  0.5, -0.5,  0.5, 1.0 ), // 3
-        vec4( -0.5, -0.5, -0.5, 1.0 ), // 4 
-        vec4( -0.5,  0.5, -0.5, 1.0 ), // 5
-        vec4(  0.5,  0.5, -0.5, 1.0 ), // 6
-        vec4(  0.5, -0.5, -0.5, 1.0 ),  // 7
-    
-        // bottom
-        vec4( -0.5, -0.5-1.0,  0.5, 1.0 ), // 0
-        vec4( -0.5,  0.5-1.0,  0.5, 1.0 ), // 1
-        vec4(  0.5,  0.5-1.0,  0.5, 1.0 ), // 2
-        vec4(  0.5, -0.5-1.0,  0.5, 1.0 ), // 3
-        vec4( -0.5, -0.5-1.0, -0.5, 1.0 ), // 4 
-        vec4( -0.5,  0.5-1.0, -0.5, 1.0 ), // 5
-        vec4(  0.5,  0.5-1.0, -0.5, 1.0 ), // 6
-        vec4(  0.5, -0.5-1.0, -0.5, 1.0 ),  // 7
-    
-        // right
-        vec4( -0.5+1.0, -0.5,  0.5, 1.0 ), // 0
-        vec4( -0.5+1.0,  0.5,  0.5, 1.0 ), // 1
-        vec4(  0.5+1.0,  0.5,  0.5, 1.0 ), // 2
-        vec4(  0.5+1.0, -0.5,  0.5, 1.0 ), // 3
-        vec4( -0.5+1.0, -0.5, -0.5, 1.0 ), // 4 
-        vec4( -0.5+1.0,  0.5, -0.5, 1.0 ), // 5
-        vec4(  0.5+1.0,  0.5, -0.5, 1.0 ), // 6
-        vec4(  0.5+1.0, -0.5, -0.5, 1.0 )  // 7
-    ]; 
-    
-    var vertexColors = [
-        [ 0.0, 0.0, 0.0, 1.0 ],  // black
-        [ 1.0, 0.0, 0.0, 1.0 ],  // red
-        [ 1.0, 1.0, 0.0, 1.0 ],  // yellow
-        [ 0.0, 1.0, 0.0, 1.0 ],  // green
-        [ 0.0, 0.0, 1.0, 1.0 ],  // blue
-        [ 1.0, 0.0, 1.0, 1.0 ],  // magenta
-        [ 0.0, 1.0, 1.0, 1.0 ],  // cyan
-        [ 1.0, 1.0, 1.0, 1.0 ]   // white
-    ];
-}
-
-var indices = [a,b,c,a,c,d]; //1 0 3, 1 3 2 // 4 5 6, 4 6 7
-
-console.log(indices)
-    for (var i = 0; i<indices.length; ++i){
-        points.push(verices[indices[i]]);
-        // colors.push(vertexColors[indices[i]]);
-
-        // for solid colored faces use
-        colors.push(vertexColors[a%8]);
-    }
-
-// Each face determines two triangles
-function colorCube()
-{
-    quad( 1, 0, 3, 2 ); // blue
-    quad( 2, 3, 7, 6 ); // yellow
-    quad( 3, 0, 4, 7 ); // green
-    quad( 6, 5, 1, 2 ); // cyan
-    quad( 4, 5, 6, 7 ); // red       
-    quad( 5, 4, 0, 1 ); // magenta
-}
-
-
-
 window.onload = function init()
 {
     canvas = document.getElementById( "gl-canvas" );
@@ -169,6 +98,78 @@ window.onload = function init()
 
 
 
+// quad uses first index to set color for face
+function quad(a,b,c,d){
+    var vertices = [
+        vec4( -0.5, -0.5,  0.5, 1.0 ), // 0
+        vec4( -0.5,  0.5,  0.5, 1.0 ), // 1
+        vec4(  0.5,  0.5,  0.5, 1.0 ), // 2
+        vec4(  0.5, -0.5,  0.5, 1.0 ), // 3
+        vec4( -0.5, -0.5, -0.5, 1.0 ), // 4 
+        vec4( -0.5,  0.5, -0.5, 1.0 ), // 5
+        vec4(  0.5,  0.5, -0.5, 1.0 ), // 6
+        vec4(  0.5, -0.5, -0.5, 1.0 ),  // 7
+    
+        // bottom
+        vec4( -0.5, -0.5-1.0,  0.5, 1.0 ), // 0
+        vec4( -0.5,  0.5-1.0,  0.5, 1.0 ), // 1
+        vec4(  0.5,  0.5-1.0,  0.5, 1.0 ), // 2
+        vec4(  0.5, -0.5-1.0,  0.5, 1.0 ), // 3
+        vec4( -0.5, -0.5-1.0, -0.5, 1.0 ), // 4 
+        vec4( -0.5,  0.5-1.0, -0.5, 1.0 ), // 5
+        vec4(  0.5,  0.5-1.0, -0.5, 1.0 ), // 6
+        vec4(  0.5, -0.5-1.0, -0.5, 1.0 ),  // 7
+    
+        // right
+        vec4( -0.5+1.0, -0.5,  0.5, 1.0 ), // 0
+        vec4( -0.5+1.0,  0.5,  0.5, 1.0 ), // 1
+        vec4(  0.5+1.0,  0.5,  0.5, 1.0 ), // 2
+        vec4(  0.5+1.0, -0.5,  0.5, 1.0 ), // 3
+        vec4( -0.5+1.0, -0.5, -0.5, 1.0 ), // 4 
+        vec4( -0.5+1.0,  0.5, -0.5, 1.0 ), // 5
+        vec4(  0.5+1.0,  0.5, -0.5, 1.0 ), // 6
+        vec4(  0.5+1.0, -0.5, -0.5, 1.0 )  // 7
+    ]; 
+    
+    var vertexColors = [
+        [ 0.0, 0.0, 0.0, 1.0 ],  // black
+        [ 1.0, 0.0, 0.0, 1.0 ],  // red
+        [ 1.0, 1.0, 0.0, 1.0 ],  // yellow
+        [ 0.0, 1.0, 0.0, 1.0 ],  // green
+        [ 0.0, 0.0, 1.0, 1.0 ],  // blue
+        [ 1.0, 0.0, 1.0, 1.0 ],  // magenta
+        [ 0.0, 1.0, 1.0, 1.0 ],  // cyan
+        [ 1.0, 1.0, 1.0, 1.0 ]   // white
+    ];
+}
+
+var indices = [a,b,c,a,c,d]; //1 0 3, 1 3 2 // 4 5 6, 4 6 7
+
+console.log(indices)
+    for (var i = 0; i<indices.length; ++i){
+        points.push(verices[indices[i]]);
+        // colors.push(vertexColors[indices[i]]);
+
+        // for solid colored faces use
+        colors.push(vertexColors[a%8]);
+    }
+
+// // Each face determines two triangles
+// function colorCube()
+// {
+//     quad( 1, 0, 3, 2 ); // blue
+//     quad( 2, 3, 7, 6 ); // yellow
+//     quad( 3, 0, 4, 7 ); // green
+//     quad( 6, 5, 1, 2 ); // cyan
+//     quad( 4, 5, 6, 7 ); // red       
+//     quad( 5, 4, 0, 1 ); // magenta
+// }
+
+
+
+
+
+
 var render=function()
 {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -176,7 +177,7 @@ var render=function()
     eye=vec3(radius*Math.cos(theta)*Math.sin(phi), radius*Math.sin(theta),
             radius*Math.cos(theta)*Math.cos(phi)); // eye point
     modelViewMatrix = lookAt(eye, at , up); 
-    projectionMatrix = ortho(left,right,bottom,ytop,near,far);
+    projectionMatrix = perspective(left,right,bottom,ytop,near,far);
 
     gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
     gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
