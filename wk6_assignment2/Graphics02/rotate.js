b9350eb18d4aa7d08acfcbdc8881b1787ddd8024
youkeongjun <youkeongjun@naver.com>
2022-10-14 18:26:21 +0900

    vertices

--- wk6_assignment2/Graphics02/rotate.js
+++ wk6_assignment2/Graphics02/rotate.js
@@ -5,3 +5,19 @@ var delay = 100;
 var direction = true;
 var intervalId;
 
+window.onload = function init()
+{
+    canvas = document.getElementById( "gl-canvas" );
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    var program = initShaders(gl,"vertex-shader","fragment-shader");
+    gl.useProgram(program);
+
+    var vertices = [vec2(0,1), vec2(-1,0), vec2(1,0), vec2(0,-1)]
+
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER,bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER,flatten(vertices),gl.STATIC_DRAW)
+    
+}
