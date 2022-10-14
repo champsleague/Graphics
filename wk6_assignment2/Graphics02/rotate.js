d8496c30f8e8409cfca916f95e591808f2af57e1
youkeongjun <youkeongjun@naver.com>
2022-10-14 22:58:56 +0900

    rotates

--- wk6_assignment2/Graphics02/rotate.js
+++ wk6_assignment2/Graphics02/rotate.js
@@ -7,17 +7,20 @@ var intervalId;
 
 window.onload = function init()
 {
-    canvas = document.getElementById( "gl-canvas" );
+    var canvas = document.getElementById( "gl-canvas" );
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(0.0,0.0,0.0,1.0);
 
     var program = initShaders(gl,"vertex-shader","fragment-shader");
     gl.useProgram(program);
 
     var vertices = [vec2(0,1), vec2(-1,0), vec2(1,0), vec2(0,-1)]
 
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER,bufferId);
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER,vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER,flatten(vertices),gl.STATIC_DRAW)
     
 
@@ -31,23 +34,24 @@ window.onload = function init()
         direction =! direction;
     }
 
-    document.getElementById("Controls").onclick = function(event){
-        switch(event.target.index){
-            case 0:
-                direction = !direction;
-                break;
-            case 1:
-                delay /= 2.0;
-                clearInterval(intervalId);
-                intervalId = setInterval(render,delay);
-                break;
-            case 2:
-                delay *= 2.0;
-                clearInterval(intervalId);
-                intervalId = setInterval(render,delay);
-                break;
+    document.getElementById("Controls" ).onclick = function(event){
+        switch( event.target.index ) { 
+        case 0: 
+            direction = !direction; 
+            break; 
+        case 1: 
+            delay /= 2.0; 
+            clearInterval(intervalId);
+            intervalId = setInterval(render, delay);
+            break; 
+        case 2: 
+            delay *= 2.0; 
+            clearInterval(intervalId);
+            intervalId = setInterval(render, delay);
+            break; 
         }
-    }
+        };
+        
     window.addEventListener("keydown",c2);
     intervalId = setInterval(render,delay)
 }
@@ -55,12 +59,12 @@ window.onload = function init()
 function render(){
     gl.clear(gl.COLOR_BUFFER_BIT);
     theta += (direction ? 0.1 : -0.1);
-    gl.uniform1f(thetaLoc,theta);
-    gl.drawArrays(gl.TRIANGLE_STRIP,0,4)
+    gl.uniform1f(thetaLoc, theta);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
     // requestAnimFrame(render)
 }
 
-function c2(){
+function c2(event){
     console.log("first event")
     console.log(event.keyCode)
     switch (event.keyCode){
